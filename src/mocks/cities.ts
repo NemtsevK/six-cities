704be61a58865f931076233ecbdbc1b31d0b8864
Konstantin <mutecross@gmail.com>
2024-02-20 00:08:54 +0300

    добавление моковых данных

---
+++ src/mocks/cities.ts
@@ -0,0 +1,44 @@
+export const cities = [
+  {
+    name: 'Amsterdam',
+    location: {
+      latitude: 52.379189,
+      longitude: 4.899431,
+    },
+  },
+  {
+    name: 'Paris',
+    location: {
+      latitude: 48.8566,
+      longitude: 2.3522,
+    },
+  },
+  {
+    name: 'Cologne',
+    location: {
+      latitude: 50.9375,
+      longitude: 6.9603,
+    },
+  },
+  {
+    name: 'Brussels',
+    location: {
+      latitude: 50.8503,
+      longitude: 4.3517,
+    },
+  },
+  {
+    name: 'Hamburg',
+    location: {
+      latitude: 53.5511,
+      longitude: 9.9937,
+    },
+  },
+  {
+    name: 'Dusseldorf',
+    location: {
+      latitude: 51.2277,
+      longitude: 6.7735,
+    },
+  },
+];
---
+++ src/mocks/users.ts
@@ -0,0 +1,39 @@
+const AVATAR_URL = 'https://i.pravatar.cc/128';
+
+export const users = [
+  {
+    name: 'Oliver Conner',
+    avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+    isPro: false,
+    email: 'Oliver.conner@gmail.com',
+    token: 'T2xpdmVyLmNvbm5lckBnbWFpbC5jb20='
+  },
+  {
+    name: 'Oliver Conner',
+    avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+    isPro: false,
+    email: 'Oliver.conner@gmail.com',
+    token: 'T2xpdmVyLmNvbm5lckBnbWFpbC5jb20='
+  },
+  {
+    name: 'Ava Johnson',
+    avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+    isPro: true,
+    email: 'ava.johnson@example.com',
+    token: 'QXZhLmpvaG5zb25AZXhhbXBsZS5jb20='
+  },
+  {
+    name: 'Liam Smith',
+    avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+    isPro: false,
+    email: 'liam.smith@example.com',
+    token: 'TGltIFNtaXRoQGV4YW1wbGUuY29t'
+  },
+  {
+    name: 'Charlotte Williams',
+    avatarUrl: `${AVATAR_URL}?rnd=${Math.random()}`,
+    isPro: true,
+    email: 'charlotte.williams@example.com',
+    token: 'Q2hhcnRvbGluZSBXaWxsaWFtc0BleGFtcGxlLmNvbQ=='
+  },
+];
