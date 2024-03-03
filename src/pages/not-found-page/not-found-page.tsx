022656a320c7649d9c9f4a64c2532a34b2acb44e
Konstantin <mutecross@gmail.com>
2024-03-03 09:08:11 +0300

    добавлены стили для 404

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,23 +1,17 @@
 import {Link} from 'react-router-dom';
 import {Helmet} from 'react-helmet-async';
-import {Header} from '../../components/header/header.tsx';
+import styles from '../../style.module.css';
+import {AppRoute} from '../../const.ts';
 
 export function NotFoundPage(): JSX.Element {
   return (
-    <div className="page page--gray page--login">
+    <div className="page">
       <Helmet>
         <title>6 cities. Page not found</title>
       </Helmet>
-      <Header isActive={false} isNav={false}/>
-      <main className="page__main page__main--login">
-        <div className="page__login-container container">
-          <section className="login form">
-            <h1 className="login__title">404. Page not found</h1>
-            <div className="login__form form">
-              <Link className="login__submit form__submit button" to="/">Go back to the main page</Link>
-            </div>
-          </section>
-        </div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>404. Page not found</h1>
+        <Link className={styles.button} to={AppRoute.Main}>Go back to the main page</Link>
       </main>
     </div>
   );
