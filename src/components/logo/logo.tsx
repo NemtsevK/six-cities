c47766bddd64fa7329b662e66fc5bf56efd0f6d8
Konstantin <mutecross@gmail.com>
2024-03-13 05:15:06 +0300

    рефакторинг

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -6,7 +6,6 @@ type LogoProps = {
 }
 
 export function Logo({isActive}: LogoProps): JSX.Element {
-
   return (
     <Link className={`header__logo-link ${isActive ? 'header__logo-link--active' : ''}`} to={AppRoute.Main}>
       <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
